5108dc7be7b9fb17e55ded08a36a8dfad7594d09
Khosiyat Sabirova <hosiyatsabirova@gmail.com>
2023-10-09 14:03:45 +0200

    Update profile.py

--- profile.py
+++ profile.py
@@ -4,48 +4,35 @@ from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 from .models import Profile, Post, Post_StartUp, Follow
 
-class UserProfileViewTest(TestCase):
-    def setUp(self):
-        # Create a test user and profile
-        self.user = User.objects.create_user(username='testuser', password='testpassword')
-        self.profile = Profile.objects.create(user=self.user)
 
-    def test_user_profile_view(self):
-        # Create some test data, e.g., posts and favorites
-        post1 = Post.objects.create(user=self.user, content='Test Post 1')
-        post2 = Post.objects.create(user=self.user, content='Test Post 2')
-        startup1 = Post_StartUp.objects.create(user=self.user, content='Startup Post 1')
-        startup2 = Post_StartUp.objects.create(user=self.user, content='Startup Post 2')
+def UserProfile(request, username):
+	user = get_object_or_404(User, username=username)
+	profile = Profile.objects.get(user=user)
+	url_name = resolve(request.path).url_name
 
-        # Simulate a request to the user profile page
-        url = reverse('profile', args=[self.user.username])
-        response = self.client.get(url)
+	# template = loader.get_template('base.html')
+	
+	if url_name == 'profile':
+		posts = Post.objects.filter(user=user).order_by('-posted')
+		posts_StartUp = Post_StartUp.objects.filter(user=user).order_by('-posted')
 
-        # Check if the response status code is 200 (OK)
-        self.assertEqual(response.status_code, 200)
+	else:
+		posts = profile.favorites.all()
+		posts_StartUp = profile.favorites_StartUp.all()
 
-        # Check if the profile information is present in the response
-        self.assertContains(response, self.profile.user.username)
+	template = loader.get_template('profile.html') 
 
-        # Check if the user's posts and startup posts are displayed
-        self.assertContains(response, 'Test Post 1')
-        self.assertContains(response, 'Test Post 2')
-        self.assertContains(response, 'Startup Post 1')
-        self.assertContains(response, 'Startup Post 2')
+	#favorite count
+	following_count = Follow.objects.filter(follower=user).count()
+	followers_count = Follow.objects.filter(following=user).count() 
 
-    def test_user_profile_view_with_followers_and_following(self):
-        # Create some followers and following relationships
-        user2 = User.objects.create_user(username='testuser2', password='testpassword')
-        Follow.objects.create(follower=self.user, following=user2)
-        Follow.objects.create(follower=user2, following=self.user)
 
-        # Simulate a request to the user profile page
-        url = reverse('profile', args=[self.user.username])
-        response = self.client.get(url)
+	context = {
+		'profile':profile,
+		'following_count':following_count,
+		'followers_count':followers_count,
 
-        # Check if the response status code is 200 (OK)
-        self.assertEqual(response.status_code, 200)
+	}
 
-        # Check if follower and following counts are displayed
-        self.assertContains(response, 'Followers: 1')
-        self.assertContains(response, 'Following: 1')
+	return HttpResponse(template.render(context, request))
+
